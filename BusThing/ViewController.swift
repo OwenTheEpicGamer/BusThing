f1cfd99b150c37dcd4ba6940e30ec2f864d7cfb2
OwenTheEpicGamer <88258850+OwenTheEpicGamer@users.noreply.github.com>
2023-03-03 18:56:43 -0500

    Rawr

---
+++ BusThing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BusThing
+//
+//  Created by Owen Guo on 2023-03-03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemCyan
+    }
+
+
+}
+
